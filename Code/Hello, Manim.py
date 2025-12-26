631f56e462707b5dde6c1c10e1005748e993ed18
Koshelev-Kirill <koshelev.kirill.pavlovich@gmail.com>
2025-12-26 19:51:53 +0300

    Update Hello, Manim.py

--- Code/Hello, Manim.py
+++ Code/Hello, Manim.py
@@ -2,15 +2,9 @@ from manim import *
 
 class FirstScene(Scene):
     def construct(self):
-        # Создаем круг
         circle = Circle(color=BLUE, fill_opacity=0.5)
-        # Создаем надпись с LaTeX
         text = MathTex(r"\text{Hello, Manim!}")
-        # Располагаем текст под кругом
         text.next_to(circle, DOWN)
-
-        # Анимация: проявление круга и текста
         self.play(Create(circle))
         self.play(Write(text))
-        # Пауза в конце
         self.wait(2)
